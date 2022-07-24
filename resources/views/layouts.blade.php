946bd9a568a343bbb94addf0bbfe493204fcda96
irpannawawi <irpannawawi.ixd@gmail.com>
2022-07-24 17:25:59 +0700

    fix sidebar button hidden

--- resources/views/layouts.blade.php
+++ resources/views/layouts.blade.php
@@ -37,10 +37,16 @@
 	<div class="wrapper">
 		<!-- Navbar -->
 		<nav class="main-header navbar navbar-expand navbar-white navbar-light">
-
+<!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+      </li>
+    </ul>
 			<!-- Right navbar links -->
 			<ul class="navbar-nav ml-auto">
 				<!-- Navbar Search -->
+
 				<li class="nav-item">
 					<a class="btn btn-danger btn-sm"  href="logout">
 						Keluar
